f8a4c5aca94702ff1311a7f5033db18c2da6b2bf
Matan <dev.muchen@gmail.com>
2024-10-09 10:39:45 -0400

    added dark mode toggle and base layout

---
+++ app/components/SearchResults.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchResults = () => {
+  return (
+    <div className="row-start-4 row-end-12 col-start-1 col-end-4 bg-slate-100 dark:bg-slate-800 p-3 rounded-md">
+      SearchResults
+    </div>
+  );
+};
+
+export default SearchResults;
