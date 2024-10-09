55fab51ba0c6abb158d3469f3fa148cce616a5cd
Matan <dev.muchen@gmail.com>
2024-10-09 11:10:23 -0400

    Update SearchResults.tsx

--- app/components/SearchResults.tsx
+++ app/components/SearchResults.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const SearchResults = () => {
   return (
     <div className="row-start-4 row-end-12 col-start-1 col-end-4 bg-slate-100 dark:bg-slate-800 p-3 rounded-md">
-      SearchResults
+      SearchResults not a real change
     </div>
   );
 };
